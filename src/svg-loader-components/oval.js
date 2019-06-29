e9d638ad0cdc654d0cefe347b1d365b08e38eb4f
Sanket Panda <pandasa123@gmail.com>
2019-06-29 12:40:48 -0500

    Made all SVGs inline with string literals. Removed `*.svg`

--- src/svg-loader-components/oval.js
+++ src/svg-loader-components/oval.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import SVGInline from 'react-svg-inline';
-import image from './oval.svg';
 
 const Oval = props => (
-  <SVGInline svg={image} {...props} />
+  <SVGInline
+    svg={`<svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff">
+  <g fill="none" fill-rule="evenodd">
+      <g transform="translate(1 1)" stroke-width="2">
+          <circle stroke-opacity=".5" cx="18" cy="18" r="18"/>
+          <path d="M36 18c0-9.94-8.06-18-18-18">
+              <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  from="0 18 18"
+                  to="360 18 18"
+                  dur="1s"
+                  repeatCount="indefinite"/>
+          </path>
+      </g>
+  </g>
+</svg>`}
+    {...props}
+  />
 );
 
 export { Oval };
