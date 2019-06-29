1557088e292952fac9893102dc7877f2696d63a7
Sanket Panda <pandasa123@gmail.com>
2019-06-29 13:20:25 -0500

    Converted SVGs, removed react-svg-inline

--- src/svg-loader-components/tail-spin.js
+++ src/svg-loader-components/tail-spin.js
@@ -1,9 +1,47 @@
 import React from 'react';
-import SVGInline from 'react-svg-inline';
-import image from './tail-spin.svg';
 
 const TailSpin = props => (
-  <SVGInline svg={image} {...props} />
+  <svg width={38} height={38} {...props}>
+    <defs>
+      <linearGradient
+        x1="8.042%"
+        y1="0%"
+        x2="65.682%"
+        y2="23.865%"
+        id="prefix__a"
+      >
+        <stop stopColor="#fff" stopOpacity={0} offset="0%" />
+        <stop stopColor="#fff" stopOpacity={0.631} offset="63.146%" />
+        <stop stopColor="#fff" offset="100%" />
+      </linearGradient>
+    </defs>
+    <g transform="translate(1 1)" fill="none" fillRule="evenodd">
+      <path
+        d="M36 18c0-9.94-8.06-18-18-18"
+        stroke="url(#prefix__a)"
+        strokeWidth={2}
+      >
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          from="0 18 18"
+          to="360 18 18"
+          dur="0.9s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <circle fill="#fff" cx={36} cy={18} r={1}>
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          from="0 18 18"
+          to="360 18 18"
+          dur="0.9s"
+          repeatCount="indefinite"
+        />
+      </circle>
+    </g>
+  </svg>
 );
 
 export { TailSpin };
--- src/svg-loader-components/three-dots.js
+++ src/svg-loader-components/three-dots.js
@@ -1,9 +1,74 @@
 import React from 'react';
-import SVGInline from 'react-svg-inline';
-import image from './three-dots.svg';
 
 const ThreeDots = props => (
-  <SVGInline svg={image} {...props} />
+  <svg width={120} height={30} fill="#fff" {...props}>
+    <circle cx={15} cy={15} r={15}>
+      <animate
+        attributeName="r"
+        from={15}
+        to={15}
+        begin="0s"
+        dur="0.8s"
+        values="15;9;15"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+      <animate
+        attributeName="fill-opacity"
+        from={1}
+        to={1}
+        begin="0s"
+        dur="0.8s"
+        values="1;.5;1"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+    </circle>
+    <circle cx={60} cy={15} r={9} fillOpacity={0.3}>
+      <animate
+        attributeName="r"
+        from={9}
+        to={9}
+        begin="0s"
+        dur="0.8s"
+        values="9;15;9"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+      <animate
+        attributeName="fill-opacity"
+        from={0.5}
+        to={0.5}
+        begin="0s"
+        dur="0.8s"
+        values=".5;1;.5"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+    </circle>
+    <circle cx={105} cy={15} r={15}>
+      <animate
+        attributeName="r"
+        from={15}
+        to={15}
+        begin="0s"
+        dur="0.8s"
+        values="15;9;15"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+      <animate
+        attributeName="fill-opacity"
+        from={1}
+        to={1}
+        begin="0s"
+        dur="0.8s"
+        values="1;.5;1"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+    </circle>
+  </svg>
 );
 
 export { ThreeDots };
