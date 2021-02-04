fdbab4e40d4487cfde4c4935df998d5a912056ad
dkress59 <28515387+dkress59@users.noreply.github.com>
2021-02-04 22:25:48 +0100

    fixes typescript export

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
-import Audio from './components/audio'
-import BallTriangle from './components/ball-triangle'
-import Bars from './components/bars'
-import Circles from './components/circles'
-import Grid from './components/grid'
-import Hearts from './components/hearts'
-import Oval from './components/oval'
-import Puff from './components/puff'
-import Rings from './components/rings'
-import SpinningCircles from './components/spinning-circles'
-import TailSpin from './components/tail-spin'
-import ThreeDots from './components/three-dots'
+import AudioIcon from './components/audio'
+import BallTriangleIcon from './components/ball-triangle'
+import BarsIcon from './components/bars'
+import CirclesIcon from './components/circles'
+import GridIcon from './components/grid'
+import HeartsIcon from './components/hearts'
+import OvalIcon from './components/oval'
+import PuffIcon from './components/puff'
+import RingsIcon from './components/rings'
+import SpinningCirclesIcon from './components/spinning-circles'
+import TailSpinIcon from './components/tail-spin'
+import ThreeDotsIcon from './components/three-dots'
+
+export const Audio = AudioIcon
+export const BallTriangle = BallTriangleIcon
+export const Bars = BarsIcon
+export const Circles = CirclesIcon
+export const Grid = GridIcon
+export const Hearts = HeartsIcon
+export const Oval = OvalIcon
+export const Puff = PuffIcon
+export const Rings = RingsIcon
+export const SpinningCircles = SpinningCirclesIcon
+export const TailSpin = TailSpinIcon
+export const ThreeDots = ThreeDotsIcon
 
 export default {
 	Audio,
